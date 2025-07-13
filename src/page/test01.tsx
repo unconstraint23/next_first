1537f726d4841aba7d06e315da12511637ca6a77
zhangrunjing <527667654@qq.com>
2025-07-13 23:13:21 +0800

    next初始了解

---
+++ src/page/test01.tsx
@@ -0,0 +1,35 @@
+import React, { memo, useEffect, useState } from 'react'
+
+type Todo = {
+  userId: number
+  id: number
+  title: string
+  completed: string
+}
+const test01 = memo(() => {
+     const [data, setData] = useState<Todo|null>(null);
+    useEffect(() => {
+      queryData()
+    }, [])
+    const queryData = () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => setData(data));
+    }
+  return (
+    <div>
+        <p className="text-2xl">
+          {data?.title}
+        </p>
+    </div>
+  )
+})
+
+// export async function getServerSideProps(context: any) {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//   const data = await res.json();
+
+//   return { props: { data } };
+// }
+
+export default test01
